b8c1ce55b76b6a8f43cfe75c5313e720107994c7
Sam Raker <sam.raker@gmail.com>
2015-05-23 17:53:23 -0400

    use iota, fix zipmap stuff

--- src/nonprofits/parse.clj
+++ src/nonprofits/parse.clj
@@ -1,29 +1,27 @@
 (ns nonprofits.parse
   (:require [clojure.string :as string]
             [clojure.java.io :as io]
-            [clojure.data.csv :refer [read-csv]]))
+            [clojure.data.csv :refer [read-csv]]
+            [iota]))
 
 (defmacro seq-read [fname & funcs]
   `(with-open [rdr (io/reader ~fname)]
      (doall (-> rdr line-seq ~@funcs))))
-    
+
 ;; filename constants
 (def resdir "resources/")
 (def contracts-fname (str resdir "NYC nonprofits with contracts.csv"))
 (def active-fname (str resdir "Active_Corporations___Beginning_1800.csv"))
 
 (def contracts-keys [:ein :name :address :code])
-;(def active-maps (let [k-line (seq-read active-fname first read-csv)
-;                       v-lines (seq-read active-fname next (partial map read-csv))]))
-                   
-;(def active-keys (map keyword 
-;                      (seq-read active-fname
-;                                first
-;                                read-csv)))
-;(def active-lines (map read-csv
-;                       (seq-read active-fname
-;                                 next)))
 
-;(def contracts-maps (map (partial zipmap contracts-keys)
-;                         (seq-read 
-;                         (with-open [rdr (io/reader contracts-fname)]))))
+(def active
+  (let [f-seq (iota/seq active-fname)
+        ks (first (read-csv (first f-seq)))]
+    (map (partial zipmap ks)
+         (mapcat read-csv (rest f-seq)))))
+
+(def contracts
+  (let [f-seq (iota/seq contracts-fname 256 \return)]
+    (map (partial zipmap contracts-keys)
+         (mapcat read-csv (rest f-seq)))))
