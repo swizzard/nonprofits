f4000d1b0eda66ecc60f8f5e37951f53d0027ae0
Sam Raker <sam.raker@gmail.com>
2015-05-23 17:53:05 -0400

    add iota

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/data.csv "0.1.2"]]
+                 [org.clojure/data.csv "0.1.2"]
+                 [iota "1.1.2"]]
   :main ^:skip-aot nonprofits.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
-  :jvm-opts ["-Xmx1g", "-Xms128m"])
+  :jvm-opts ["-Xmx2g", "-Xms256m"])
